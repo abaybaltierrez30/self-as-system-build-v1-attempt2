95b3679cfef1e7e6fcc479c7c80511d50d3bbd8f
Carlos <carlos@example.com>
2026-08-24 12:39:08 -0700

    Add main.js starter

---
+++ main.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+// main.js - entry point for Visual-Rule-Test
+
+function main() {
+  console.log('Visual Rule Test - main');
+  // TODO: implement main program logic for your assignment
+}
+
+if (require.main === module) main();
+
+module.exports = { main };
